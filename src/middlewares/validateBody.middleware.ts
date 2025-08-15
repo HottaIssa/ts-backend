3884fecd7d3ae525a4c2ec7918ebff3b24dacf94
HottaIssa <ostosissa@gmail.com>
2025-08-15 17:40:36 -0500

    feat: validation middleware with schemas

---
+++ src/middlewares/validateBody.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodType } from "zod";
+
+export const ValidateBody =
+  (schema: ZodType) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json({ status: "error", ...result.error.issues });
+    }
+    next();
+  };
